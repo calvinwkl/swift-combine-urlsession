6c82234a7c89770706174828f17143939d4714ac
Calvin Wong <kalokwong9@gmail.com>
2020-03-08 00:56:58 +0800

    Add logic and CombineDataSources

--- combine.urlsession/combine.urlsession/ViewController.swift
+++ combine.urlsession/combine.urlsession/ViewController.swift
@@ -7,14 +7,66 @@
 //
 
 import UIKit
+import Combine
+import CombineDataSources
+
+struct Place: Decodable, Hashable {
+    let name: String
+    let desc: String
+    let url: String
+}
 
 class ViewController: UIViewController {
+    
+    let tableView = UITableView()
+    var places = PassthroughSubject<[Place], Never>()
+    var subscriptions = [AnyCancellable]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Travel"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        view.addSubview(tableView)
+        view.backgroundColor = .white
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0.0),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0.0),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0.0),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0.0)
+        ])
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        places.bind(subscriber:
+            tableView.rowsSubscriber(
+                cellIdentifier: "cell",
+                cellType: UITableViewCell.self,
+                cellConfig: { cell, indexPath, model in
+                    cell.textLabel?.text = "\(model.name), \(model.desc)"
+                }
+            )
+        )
+        .store(in: &subscriptions)
+                
+        fetchData()
     }
 
+    func fetchData() {
+        if let url = URL(string: "https://api.myjson.com/bins/16w6h0") {
+            URLSession.shared.dataTaskPublisher(for: url)
+                .map { $0.data }
+                .decode(type: [Place].self, decoder: JSONDecoder())
+                .receive(on: RunLoop.main)
+                .sink(receiveCompletion: { completion in
+                    print(completion) // finished
+                }) { place in
+                    self.places.send(place)
+                }.store(in: &subscriptions)
+        }
+    }
 
 }
 
